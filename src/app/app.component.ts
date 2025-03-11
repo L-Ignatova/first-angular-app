2b7851abcb865b47a7dab7727b888b5842569676
Liliya Ignatova <liliya.oignatova@gmail.com>
2025-03-11 16:33:45 +0200

    add tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 // adds metadata to the class
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
   title = 'first-angular-app';
+  selectedUserId = 'u1';
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
+  }
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,15 @@
-import { Component, computed, input, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 
 @Component({
   selector: 'app-user',
@@ -11,24 +22,26 @@ export class UserComponent {
   // signal stores the state of a value and monitors
   // all instances of the value in the app, updates app on change
   // selectedUser = signal(DUMMY_USERS[this.randomIndex]);
+  // this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
 
   // computed - smth like useMemo - changes when the
   // signals used inside of it change
   // not everytime the component or the app changes
- 
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>(); // emit custom values through SELECT to any parent component that is interested
+
   // SIGNALS approach
   // avatar = input.required<string>();
   // name = input.required<string>();
   // imagePath = computed(() => 'assets/users/' + this.avatar());
+  // select = output<string>(); // explicitly creates EventEmitter under hood
 
   onUserClick() {
-    console.log('clicked!');
-    // this.selectedUser.set(DUMMY_USERS[this.randomIndex]);
+    this.select.emit(this.user.id);
   }
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 }
